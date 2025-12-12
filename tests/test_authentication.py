814c2d02358f66b6c6f28184838f5aac0dcdbc7d
emilkabals <esamigyllin@gmail.com>
2025-12-12 20:57:22 +0300

    Исправление критических ошибок в тестах
    - Убраны антипаттерны WebDriverWait вне assert
    - Исправлена логика проверок (убраны лишние and is_displayed())
    - Завершены незаконченные функции
    - Все ожидания перенесены внутрь assert
    - Использованы структурированные данные из data.data

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -5,27 +5,21 @@ from data.data import UsersTestData
 
 
 class TestAuthentication:
-    # Вход по кнопке «Войти в аккаунт» на главной
     def test_authentication_by_button_login_in_main_page_success(self, driver):
+        # ARRANGE: Подготовка (минимальная)
+        email = UsersTestData.VALID_USER['email']
+        password = UsersTestData.VALID_USER['password']
+        
+        # исправления в коде: Действия (прямые и лаконичные)
         driver.find_element(*Locators.button_login_in_main).click()
-        WebDriverWait(driver, 6).until(expected_conditions.visibility_of_element_located(
-            Locators.button_register))
+        driver.find_element(*Locators.input_email_auth).send_keys(email)
+        driver.find_element(*Locators.input_password_auth).send_keys(password)
+        driver.find_element(*Locators.button_login).click()
         
-        email_element = driver.find_element(*Locators.input_email_auth)
-        password_element = driver.find_element(*Locators.input_password_auth)
-        login_button = driver.find_element(*Locators.button_login)
-        make_order_button = Locators.button_make_the_order
-
-        email_element.send_keys(UsersTestData.email)
-        password_element.send_keys(UsersTestData.password)
-        login_button_element = driver.find_element(*Locators.button_login)
-        login_button_element.click()
-
-        # Ожидание появления кнопки "Сделать заказ"
-        WebDriverWait(driver, 6).until(expected_conditions.visibility_of_element_located(make_order_button))
-        
-        # Ассерты с поиском элемента внутри
-        assert driver.find_element(*make_order_button).is_displayed()
+        # перенос в ASSERT: Проверка (с ожиданием ВНУТРИ assert)
+        assert WebDriverWait(driver, 6).until(
+            EC.visibility_of_element_located(Locators.button_make_the_order)
+        ).is_displayed()
 
 class TestAuthentication:
     # Вход через кнопку в форме регистрации
@@ -41,23 +35,31 @@ class TestAuthentication:
             expected_conditions.visibility_of_element_located(Locators.button_make_the_order)
         ).is_displayed()
         
+class TestAuthentication:
     # Вход через кнопку «Личный кабинет»
     def test_authentication_by_button_personal_account_in_main_page_success(self, driver):
+        # измененный код
         driver.find_element(*Locators.button_personal_account).click()
-        WebDriverWait(driver, 6).until(expected_conditions.visibility_of_element_located(Locators.button_register))
-        driver.find_element(*Locators.input_email_auth).send_keys(UsersTestData.email)
-        driver.find_element(*Locators.input_password_auth).send_keys(UsersTestData.password)
+        driver.find_element(*Locators.input_email_auth).send_keys(UsersTestData.VALID_USER['email'])
+        driver.find_element(*Locators.input_password_auth).send_keys(UsersTestData.VALID_USER['password'])
         driver.find_element(*Locators.button_login).click()
-        # Переносим ожидание и поиск внутрь ассерта
-        assert WebDriverWait(driver, 6).until(
-            expected_conditions.visibility_of_element_located(Locators.button_make_the_order)
-        ) and driver.find_element(*Locators.button_make_the_order).is_displayed()
+        
+        # ASSERT (исправлено)
+        assert WebDriverWait(driver, UsersTestData.TIMEOUTS['medium']).until(
+            EC.visibility_of_element_located(Locators.button_make_the_order)
+        )  # WebDriverWait.until() уже возвращает элемент или бросает исключение
 
-    # Вход через кнопку в форме регистрации
+    # Вход через кнопку в форме регистрации  
     def test_authentication_by_button_login_in_registration_form_success(self, driver):
+        # измененный код
         driver.find_element(*Locators.button_login_in_main).click()
-        WebDriverWait(driver, 6).until(expected_conditions.visibility_of_element_located(Locators.button_register))
         driver.find_element(*Locators.button_register).click()
-        WebDriverWait(driver, 6).until(expected_conditions.visibility_of_element_located(Locators.button_submit))
         driver.find_element(*Locators.button_login_in_registration_form).click()
-        driver.find_element
+        driver.find_element(*Locators.input_email_auth).send_keys(UsersTestData.VALID_USER['email'])
+        driver.find_element(*Locators.input_password_auth).send_keys(UsersTestData.VALID_USER['password'])
+        driver.find_element(*Locators.button_login).click()
+        
+        # ASSERT
+        assert WebDriverWait(driver, UsersTestData.TIMEOUTS['medium']).until(
+            EC.visibility_of_element_located(Locators.button_make_the_order)
+        )
